10ec586303ca6bf3d343aaa70626adc6b1595ee3
niku <niku@niku.name>
2018-10-07 17:44:15 +0900

    Import project
    $ mix new protobuf

---
+++ lib/protobuf.ex
@@ -0,0 +1,18 @@
+defmodule Protobuf do
+  @moduledoc """
+  Documentation for Protobuf.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Protobuf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
