9dc39bcf030d3843269b69e7724ed1a0f07610e9
niku <niku@niku.name>
2018-10-09 13:03:50 +0900

    Implement Ptotobuf.decode_field_number_and_value/1

--- lib/protobuf.ex
+++ lib/protobuf.ex
@@ -2,4 +2,58 @@ defmodule Protobuf do
   @moduledoc """
   Documentation for Protobuf.
   """
+
+  def decode_varint(binary) when is_binary(binary) do
+    # See https://developers.google.com/protocol-buffers/docs/encoding#varints
+    {bits, rest} = do_decode_varint(binary, <<>>)
+    bit_size = bit_size(bits)
+    # Convert from bitstrings to integer
+    <<number::integer-size(bit_size)>> = bits
+    {number, rest}
+  end
+
+  defp do_decode_varint(<<0::1, lower7bits::bitstring-size(7), rest::binary>>, accumulator) do
+    {<<lower7bits::bitstring-size(7), accumulator::bitstring>>, rest}
+  end
+
+  defp do_decode_varint(<<1::1, lower7bits::bitstring-size(7), rest::binary>>, accumulator) do
+    do_decode_varint(rest, <<lower7bits::bitstring-size(7), accumulator::bitstring>>)
+  end
+
+  def decode_field_number_and_value(binary) when is_binary(binary) do
+    # See https://developers.google.com/protocol-buffers/docs/encoding#structure
+    {bits, rest} = do_decode_varint(binary, <<>>)
+    wire_type_size = 3
+    field_size = bit_size(bits) - wire_type_size
+    # Convert from bitstrings to integer
+    <<field_number::integer-size(field_size), wire_type::integer-size(wire_type_size)>> = bits
+
+    {value, rest2} =
+      case wire_type do
+        0 ->
+          decode_varint(rest)
+
+        1 ->
+          # TODO
+          nil
+
+        2 ->
+          # TODO
+          nil
+
+        3 ->
+          # TODO
+          nil
+
+        4 ->
+          # TODO
+          nil
+
+        5 ->
+          # TODO
+          nil
+      end
+
+    {field_number, value, rest2}
+  end
 end
--- test/protobuf_test.exs
+++ test/protobuf_test.exs
@@ -1,8 +1,28 @@
 defmodule ProtobufTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Protobuf
 
-  test "greets the world" do
-    assert Protobuf.hello() == :world
+  def sigil_b(term, _modifiers) do
+    for <<i <- term>>, i in [?0, ?1], do: <<i::1>>, into: <<>>
+  end
+
+  def sigil_B(term, modifiers) do
+    sigil_b(term, modifiers)
+  end
+
+  describe "decode_varint/1" do
+    test "0000_0001 converts to 1" do
+      assert {1, <<>>} = Protobuf.decode_varint(~b(0000_0001))
+    end
+
+    test "1010_1100_0000_0010 converts to 300" do
+      assert {300, <<>>} = Protobuf.decode_varint(~b(1010_1100_0000_0010))
+    end
+  end
+
+  describe "decode_field_number_and_value/1" do
+    test "0000_1000_1001_0110_0000_0001 converts 1 as a field and 150 as a value" do
+      assert {1, 150, <<>>} = Protobuf.decode_field_number_and_value(~b(0000_1000_1001_0110_0000_0001))
+    end
   end
 end
