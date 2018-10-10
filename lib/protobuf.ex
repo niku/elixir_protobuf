0f1e8f097dd42cdbf2b270a83e3bde76808c3ee1
niku <niku@niku.name>
2018-10-11 08:59:56 +0900

    Remove provisional function

--- lib/protobuf.ex
+++ lib/protobuf.ex
@@ -2,17 +2,4 @@ defmodule Protobuf do
   @moduledoc """
   Documentation for Protobuf.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Protobuf.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
